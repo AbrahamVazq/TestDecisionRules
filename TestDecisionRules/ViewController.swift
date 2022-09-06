97d44faecaf8e6ba216d931973096a818af9e855
Moises Abraham Vazquez Perez <mavazquezp@bancoazteca.com>
2022-09-06 12:31:34 -0500

    Initial Commit

---
+++ TestDecisionRules/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestDecisionRules
+//
+//  Created by 291732 on 06/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
