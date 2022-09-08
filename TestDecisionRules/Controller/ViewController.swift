3bae0f6f8e384ca3861215693064a1cbe08f0ed8
Moises Abraham Vazquez Perez <mabraham.vazquez@gmail.com>
2022-09-08 15:34:50 -0500

    [UPDATE] Actualización en pantalla de login

--- TestDecisionRules/Controller/ViewController.swift
+++ TestDecisionRules/Controller/ViewController.swift
@@ -14,14 +14,16 @@ class ViewController: UIViewController {
     }
     @IBOutlet weak var btnLogin: UIButton!
     
-    @IBOutlet weak var lblUser: UITextField!{
+    @IBOutlet weak var txfUser: UITextField!{
         didSet{
-            self.lblUser.textColor = UIColor(red: 70/255, green: 0/255, blue: 225/255, alpha: 1)
+            self.txfUser.delegate = self
+            self.txfUser.textColor = UIColor(red: 70/255, green: 0/255, blue: 225/255, alpha: 1)
         }
     }
-    @IBOutlet weak var lblPass: UITextField!{
+    @IBOutlet weak var txfPass: UITextField!{
         didSet{
-            self.lblPass.textColor = UIColor(red: 70/255, green: 0/255, blue: 225/255, alpha: 1)
+            self.txfPass.delegate = self
+            self.txfPass.textColor = UIColor(red: 70/255, green: 0/255, blue: 225/255, alpha: 1)
         }
     }
     
@@ -30,29 +32,20 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
     
-
     //MARK: - D E C I S I O N · R U L E S
     func loadServices(WithUser usr:String, andSsap ssap:String) {
         let url = URL(string: "https://api.decisionrules.io/rule/solve/52c13b20-796f-7893-38b3-ca1252495f79/1")!
         var request = URLRequest(url: url)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("Bearer BGyRbnPhxqk5BYsP1g3zt0qRL5kIf13ul95jPRh8FH0C3f9YbcRHzdUrQbGqa4Az", forHTTPHeaderField: "Authorization")
-        
         request.httpMethod = "POST"
-        //            let parameters: [String: Any] = [ "data" : [
-        //                "usuario": "Marco",
-        //                "contrasena": "03983", "esNuevoIngreso": "false"] ]
-        
+
         let parameters: [String: Any] = [ "data" : [
                         "usuario": "\(usr)",
                         "contrasena": "\(ssap)", "esNuevoIngreso": "false"] ]
         
-        
-        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n parameters ----> \(parameters) \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-        
-        
         let jsonData =  try? JSONSerialization.data(withJSONObject: parameters)
-        request.httpBody = jsonData//parameters.percentEncoded()
+        request.httpBody = jsonData
         
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             guard let data = data, let response = response as? HTTPURLResponse, error == nil else { // check for fundamental networking error
@@ -66,14 +59,9 @@ class ViewController: UIViewController {
                 return
             }
             
-            // do whatever you want with the data, e.g.:
-            
             do {
-                // let responseObject = try JSONDecoder().decode(ResponseObject<Foo>.self, from: data)
-                //print(responseObject)
                 if let json = try JSONSerialization.jsonObject(with: data, options: [.allowFragments]) as? [String : Any] {
                     print("Info Json \(json)")
-                    
                 } else {
                     let str = String(decoding: data, as: UTF8.self)
                     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n str ----> \(str) \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
@@ -83,7 +71,6 @@ class ViewController: UIViewController {
                 
             } catch {
                 print(error) // parsing error
-                
                 if let responseString = String(data: data, encoding: .utf8) {
                     print("responseString = \(responseString)")
                 } else {
@@ -91,16 +78,36 @@ class ViewController: UIViewController {
                 }
             }
         }
-        
         task.resume()
-        
     }
     
     
+    //MARK: - F U N C T I O N S
+    func showSimpleAlert() {
+        let alert = UIAlertController(title: "DecisionRules", message: "Ingresa los datos por favor", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Aceptar", style: .default, handler: nil))
+        self.present(alert, animated: true) 
+    }
+    
+    
+    //MARK: - V A L I D A T I O N S
+
+    
+    //MARK: - A C T I O N S
     @IBAction func goToLogin(_ sender: Any) {
-        loadServices(WithUser: lblUser.text ?? "", andSsap: lblPass.text ?? "")
+        
+        if txfUser.text == "" && txfPass.text == ""  {
+            self.showSimpleAlert()
+        }else{
+            loadServices(WithUser: txfUser.text ?? "", andSsap: txfPass.text ?? "")
+        }
+        
     }
     
 }
 
-//UIColor(red: 70, green: 0, blue: 225, alpha: 1).cgColor
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+    }
+}
---
+++ TestDecisionRules/Extension/Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Extension.swift
+//  TestDecisionRules
+//
+//  Created by 291732 on 08/09/22.
+//
+
+import Foundation
+
