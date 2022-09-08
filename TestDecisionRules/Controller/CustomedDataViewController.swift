c25efc89ff5d4ec8192371b94d9def2b1ad39af7
Moises Abraham Vazquez Perez <mabraham.vazquez@gmail.com>
2022-09-08 15:46:13 -0500

    [ADD ] adición del controlador para la pantalla siguiente después de login

---
+++ TestDecisionRules/Controller/CustomedDataViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CustomedDataViewController.swift
+//  TestDecisionRules
+//
+//  Created by 291732 on 08/09/22.
+//
+
+import UIKit
+
+class CustomedDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
