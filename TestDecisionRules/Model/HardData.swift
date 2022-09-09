6d03a7421e7b8ea3a83efe93708369845b7ae6ae
Abraham Vazquez <mabraham.vazquez@gmail.com>
2022-09-09 14:23:00 -0500

    [ADD] Addition de Datos * Creación de estructura

---
+++ TestDecisionRules/Model/HardData.swift
@@ -0,0 +1,25 @@
+//
+//  HardData.swift
+//  TestDecisionRules
+//
+//  Created by Abraham Vazquez on 09/09/22.
+//
+
+import Foundation
+
+struct HardData {
+    
+    
+    let arrArtistas: [String] = ["Los Acosta","The Weekend","Daft Punk","Deadmau5","Franz Ferdinand","Post Malone","Gorillaz","DaftPunk","The Strokes","Empire of the Sun"]
+
+    let arrAlbumName: [String] = ["CorazonesSolitarios","BlindingLights","Discovery","forLackOfABetterName","FranzFerdinand","HollywoodsBleeding","PlasticBeach","RAM","RoomOnFire","WalkingOnADream"]
+
+    let arrSongs: [String] = ["Como una Novela","Blinding Lights","One More Time","Gosht Stuff","This Fire","Enemies","Stylo","Instant Crush","Reptillia","Walking on a Dream","","","",""]
+    
+    
+    let arrMovies: [String] = ["Aladin","AngryBirds","AntesDeTi","Batman","BlackPhone","CapitanAmerica","Cazadores","Chihuahua","DamaYVagabundo","DragonBall","JojoRabit","PawPatrol","PorSiempre","Sirenita","SonicDos","TresGarcia"]
+
+
+    let descripcion: [String] = ["Un clasico de Disney, nos trae una pelicula de un desgraciado con suerte.","Pajarracos de mal humor, que le tienen mucho hate a puercos radioactivos","Una movie de amor para las damas, pero aburrimiento para caballeros","Un hombre millonario, emo, pero con sed de justicia, un mayordomo como tutor", "Una de esas movies que le llaman de arte por que la hizo alguien famoso, pero en realidad no rifa","Un EstadoUnidense con esteroides que lucha por el bien, pero es medio menso para juzgar", "la neta no se de que va esta movie, no la eh visto y no creo verla :v","Perros malvados, histericos y comprimidos, se supone que seran heroes gracias al poder del guion","Una pelicula de disney que tambien es para el publico femenino, es amar te duele, pero con perros","El anime mas exitoso y mas de hueva de todo el planeta, genera mas hype que los BTS, que tambien son asiaticos","Una historia mas, para empatizar sobre el conflicto nazi, a pesar de que no salimos de nuestras colonias :( F en el chat","Perritos animados que son heroes, le gusta mucho a los morrillos, y  los entretiene mucho","Una movie mas de amor, lo que el cine contemporaneo esperaba .... ok not, nomas es pa entretener ... y ni eso.","Una historia mal contada, pero animada, esta chido el final, pero puede ser peor :v ","Nos saltamos la movie 1 por que no encontramos el asset, pero esta movie esta dos tres, es para estar con la pareja comiendo palomas","La tipica pelicula que ponen en el 9, o que tu madrecita santa pondria: Por que esta chida"]
+    
+}
