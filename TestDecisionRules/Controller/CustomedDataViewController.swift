75230dcb6fc49ad5df46888e746727e224a484c0
Abraham Vazquez <mabraham.vazquez@gmail.com>
2022-09-09 13:19:06 -0500

    [UPDATE] Se actualiza el controlador de Bienvenida * Adicion de tabla * Adicion de Label de description * Control de Colores por DecisionRules

--- TestDecisionRules/Controller/CustomedDataViewController.swift
+++ TestDecisionRules/Controller/CustomedDataViewController.swift
@@ -10,24 +10,52 @@ class CustomedDataViewController: UIViewController {
         didSet{self.lblTitle.textColor = .white }
     }
     
+    @IBOutlet weak var lblDescription: UILabel!{
+        didSet{self.lblDescription.textColor = .white}
+    }
+    
+    @IBOutlet weak var tblProducts: UITableView! {
+        didSet{
+            //self.tblProducts.delegate = self
+            //self.tblProducts.dataSource = self
+            self.tblProducts.layer.cornerRadius = 15
+        }
+    }
+    
     //MARK: - V A R I A B L E S
     var user: Usuario?
     
     //MARK: - L Y F E · C Y C L E
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("\n\n\t usuario  ---> \(user.debugDescription) \n")
+        self.title = "baz x DecisionRules"
+        self.lblTitle.text = "Hola \(user?.strName ?? "Hola de nuevo!!!") !!!"
         self.setScreen(fromUser: user ?? Usuario())
     }
     
     private func setScreen(fromUser usr: Usuario) {
-        self.lblTitle.text = "Bienvenido (a) \(usr.strName ?? "")"
         self.view.backgroundColor = returnColor(WithUser: usr)
-        
+        var msg:String =  ""
+        returnSeason(FromUser: usr) != "" ? (msg = "Ya estamos en \(returnSeason(FromUser: usr)) aprovecha las ofertas que tenemos para ti! ") : (msg = "Tenemos los mejors productos! ")
+        self.lblDescription.text = msg
+    }
+    
+    func returnSeason(FromUser usr: Usuario) -> String{
+        switch usr.color{
+        case "spring":
+            return "Primavera"
+        case "summer":
+            return "Verano"
+        case "autumn":
+            return "Otoño"
+        case "winter":
+            return "Invierno"
+        default:
+            return ""
+        }
     }
     
     private func returnColor(WithUser usr: Usuario) -> UIColor {
-        print(usr.color)
         switch usr.color {
         case "pink":
             return UIColor.systemPink
@@ -41,6 +69,14 @@ class CustomedDataViewController: UIViewController {
             return UIColor.blue
         case "red":
             return UIColor.red
+        case "spring":
+            return UIColor(red: 98/255, green: 119/255, blue: 83/255, alpha: 1)
+        case "summer":
+            return UIColor(red: 247/255, green: 187/255, blue: 18/255, alpha: 1)
+        case "autumn":
+            return UIColor(red: 186/255, green: 137/255, blue: 93/255, alpha: 1)
+        case "winter":
+            return UIColor(red: 108/255, green: 200/255, blue: 236/255, alpha: 1)
         default:
             return UIColor.white
         }
